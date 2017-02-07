06d120965aee3afa8c2d38e182612695e2f1dd48
Tim Fisken <contact@timfisken.org>
2017-02-07 17:27:07 +0000

    Add implementation for week 5 exercise 3c
    Converting Director and Film to case classes means I no longer need
    to provide apply methods in the companion objects, nor do I need to
    provide the copy and equals methods in Film (because these are
    automatically provided for case classes).

--- exercises/week05/src/main/scala-2.12/Director.scala
+++ exercises/week05/src/main/scala-2.12/Director.scala
@@ -1,14 +1,11 @@
 /**
   * A class representing information about a director
   */
-class Director(val firstName: String, val lastName: String, val yearOfBirth: Int) {
+case class Director(val firstName: String, val lastName: String, val yearOfBirth: Int) {
   def name = firstName + " " + lastName
 }
 
 object Director {
-  def apply(firstName: String, lastName: String, yearOfBirth: Int) =
-    new Director(firstName, lastName, yearOfBirth)
-
   def older(first: Director, second: Director) =
     if (first.yearOfBirth < second.yearOfBirth)
       first
--- exercises/week05/src/main/scala-2.12/Film.scala
+++ exercises/week05/src/main/scala-2.12/Film.scala
@@ -1,31 +1,13 @@
 /**
   * A class representing information about a film
   */
-class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
+case class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
   def directorsAge: Int = yearOfRelease - director.yearOfBirth
 
   def isDirectedBy(query: Director): Boolean = query == director
-
-  def copy(name: String = name,
-           yearOfRelease: Int = yearOfRelease,
-           imdbRating: Double = imdbRating,
-           director: Director = director) =
-    new Film(name, yearOfRelease, imdbRating, director)
-
-  override def equals(other: Any): Boolean = other match {
-    case otherFilm: Film =>
-      name == otherFilm.name &&
-      yearOfRelease == otherFilm.yearOfRelease &&
-      imdbRating == otherFilm.imdbRating &&
-      director == otherFilm.director
-    case _ => false
-  }
 }
 
 object Film {
-  def apply(name: String, yearOfRelease: Int, imdbRating: Double, director: Director) =
-    new Film(name, yearOfRelease, imdbRating, director)
-
   def highestRating(first: Film, second: Film) =
     if (first.imdbRating > second.imdbRating)
       first
