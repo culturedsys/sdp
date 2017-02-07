03c63f878f8fe09945959c207129f81bf34d3017
Tim Fisken <contact@timfisken.org>
2017-02-07 14:56:57 +0000

    Use copy in implementing week 5 exercise 1c
    As per the instructions, use copy rather than directly creating a new instance.

--- exercises/week05/src/main/scala-2.12/Counter.scala
+++ exercises/week05/src/main/scala-2.12/Counter.scala
@@ -2,6 +2,10 @@
   * A class encapsulating the behaviour of being incremented and decremented
   */
 case class Counter(count: Int = 0) {
-  def inc(increment: Int = 1): Counter = new Counter(count + increment)
-  def dec(decrement: Int = 1): Counter = new Counter(count - decrement)
+  def inc(increment: Int = 1): Counter = copy(count = count + increment)
+  def dec(decrement: Int = 1): Counter = copy(count = count - decrement)
+}
+
+class Adder(amount: Int) {
+  def add(in: Int) = in + amount
 }
