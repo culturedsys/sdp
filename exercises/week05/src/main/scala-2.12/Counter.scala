a4bc66fed6019a3559066493faaef9178fcd0f02
Tim Fisken <contact@timfisken.org>
2017-02-07 14:51:32 +0000

    Add implementation and tests for exercise 1c

--- exercises/week05/src/main/scala-2.12/Counter.scala
+++ exercises/week05/src/main/scala-2.12/Counter.scala
@@ -1,9 +1,7 @@
 /**
   * A class encapsulating the behaviour of being incremented and decremented
   */
-class Counter(value: Int) {
+case class Counter(count: Int = 0) {
   def inc(increment: Int = 1): Counter = new Counter(count + increment)
   def dec(decrement: Int = 1): Counter = new Counter(count - decrement)
-
-  def count = value
 }
--- exercises/week05/src/test/scala-2.12/CounterTest.scala
+++ exercises/week05/src/test/scala-2.12/CounterTest.scala
@@ -8,27 +8,27 @@ class CounterTest extends FunSpec {
   describe("Counter") {
     it("has an initial value") {
       val initial = 10
-      val counter = new Counter(initial)
+      val counter = Counter(initial)
       assert(counter.count == initial)
     }
 
     it("can produce an incremented counter") {
       val initial = 10
-      val counter = new Counter(initial).inc()
+      val counter = Counter(initial).inc()
 
       assert(counter.count == initial + 1)
     }
 
     it("can produce a decremented counter") {
       val initial = 10
-      val counter = new Counter(initial).dec()
+      val counter = Counter(initial).dec()
 
       assert(counter.count == initial - 1)
     }
 
     it("is unchanged when an incremented counter is produced") {
       val initial = 10
-      val counter = new Counter(initial)
+      val counter = Counter(initial)
       counter.inc()
 
       assert(counter.count == initial)
@@ -36,7 +36,7 @@ class CounterTest extends FunSpec {
 
     it("is unchanged when a decremented counter is produced") {
       val initial = 10
-      val counter = new Counter(initial)
+      val counter = Counter(initial)
       counter.dec()
 
       assert(counter.count == initial)
@@ -45,16 +45,19 @@ class CounterTest extends FunSpec {
     it("can produce a counter incremented by more than 1") {
       val initial = 10
       val increment = 5
-      val counter = new Counter(initial).inc(increment)
+      val counter = Counter(initial).inc(increment)
       assert(counter.count == initial + increment)
     }
 
     it("can produce a counter decremented by more than 1") {
       val initial = 10
       val decrement = 5
-      val counter = new Counter(initial).dec(decrement)
+      val counter = Counter(initial).dec(decrement)
       assert(counter.count == initial - decrement)
     }
 
+    it("has a default count of 0") {
+      assert(Counter().count == 0)
+    }
   }
 }
