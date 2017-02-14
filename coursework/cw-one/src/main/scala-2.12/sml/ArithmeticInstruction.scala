dbd54d767d6be794eb498d968e9816385c0a6571
Tim Fisken <contact@timfisken.org>
2017-02-14 14:29:22 +0000

    Refactor common arithmetic instruction code
    Creates an ArithmeticInstruction class that performs a supplied arithmetic operation, to use as a base class for Add, Sub, etc.

---
+++ coursework/cw-one/src/main/scala-2.12/sml/ArithmeticInstruction.scala
@@ -0,0 +1,33 @@
+package sml
+
+/**
+  * Common base class for arithmetic instructions
+  *
+  * @param label    the label of this instruction
+  * @param opcode   the name of the opcode this instruction represents
+  * @param result   the register number in which to store the result
+  * @param operand1 the register number from which to read the left operand
+  * @param operand2 the register number from which to read the right operandf
+  * @param operation  the operation to apply to the two operands
+  */
+class ArithmeticInstruction(label: String, opcode: String,
+                            result: Int, operand1: Int, operand2: Int,
+                            operation: (Int, Int) => Int)
+  extends Instruction(label, opcode) {
+
+  /**
+    * Apply the supplied operation to the values in the given operand registers, and store the result in the given
+    * result register
+    * 
+    * @param m
+    */
+  override def execute(m: Machine) {
+    val value1 = m.regs(operand1)
+    val value2 = m.regs(operand2)
+    m.regs(result) = operation(value1, value2)
+  }
+
+  override def toString(): String = {
+    super.toString + " " + operand1 + " + " + operand2 + " to " + result
+  }
+}
