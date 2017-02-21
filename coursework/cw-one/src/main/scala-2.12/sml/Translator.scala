f1d58bb7c33d7eba7983e7e4de529349c1225bec
Tim Fisken <contact@timfisken.org>
2017-02-21 16:28:17 +0000

    cw-one: Remove no longer needed instruction name constants
    As the instructions are now being created via reflection, the Translator no longer
    needs to know about the names of the instructions.

--- coursework/cw-one/src/main/scala-2.12/sml/Translator.scala
+++ coursework/cw-one/src/main/scala-2.12/sml/Translator.scala
@@ -6,14 +6,6 @@ import java.lang.reflect.Modifier
  * The translator of a <b>S</b><b>M</b>al<b>L</b> program.
  */
 class Translator(fileName: String) {
-  private final val ADD = "add"
-  private final val LIN = "lin"
-  private final val BNZ = "bnz"
-  private final val MUL = "mul"
-  private final val DIV = "div"
-  private final val SUB = "sub"
-  private final val OUT = "out"
-
   /**
     * Translate an instruction name, as used in the input, to a class name. Note that this always produces
     * fully-qualified names in the "sml" package.
