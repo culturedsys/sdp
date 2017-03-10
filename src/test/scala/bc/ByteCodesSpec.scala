7fd978fd582ed601b8843b24d3c3eb71541044ca
Tim Fisken <contact@timfisken.org>
2017-03-10 17:11:00 +0000

    Change byte code tests to use FunSpec format
    The nested spec style seems more natural for a set of tests on a number
    of different bytecodes.

---
+++ src/test/scala/bc/ByteCodesSpec.scala
@@ -0,0 +1,60 @@
+package bc
+
+import org.scalatest.FunSpec
+
+/**
+  * Test the definition and operation of the various bytecodes
+  */
+class ByteCodeSpec extends FunSpec with ByteCodeValues {
+  describe("iconst") {
+    val iconst = new IConst(4)
+
+    it("should have the correct code value") {
+      assert(iconst.code === bytecode("iconst"))
+    }
+
+    it("should add the correct value to stack") {
+      val vm = new VirtualMachineMock(Vector.empty)
+      val value = 4
+      val iconst = new IConst(value)
+
+      assert(iconst.execute(vm).state.head === value)
+    }
+  }
+
+  val first = 3
+  val second = 5
+  val vm = new VirtualMachineMock(Vector(first, second))
+
+  describe("iadd") {
+    val iadd = new IAdd
+
+    it("should have the correct code value") {
+      assert(iadd.code === bytecode("iadd"))
+    }
+
+    it("should push the correct sum") {
+      assert(iadd.execute(vm).state.head === first + second)
+    }
+
+    it("should remove its operands from the stack") {
+      assert(iadd.execute(vm).state.tail.isEmpty)
+    }
+  }
+
+  describe("isub") {
+    val isub = new ISub
+
+    it("should have the correct code value") {
+      assert(isub.code === bytecode("isub"))
+    }
+
+    it("should push the correct subtraction") {
+      assert(isub.execute(vm).state.head === first - second)
+    }
+
+    it("should remove its operands from the stack") {
+      assert(isub.execute(vm).state.tail.isEmpty)
+    }
+  }
+}
